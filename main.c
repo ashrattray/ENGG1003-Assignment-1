82350040a7110785ddc84c3a674fb9045ee97abd
Ashleigh Rattray <ashleigh.rattray@uon.edu.au>
2019-04-03 05:19:41 +0000

    master: main.c new - inserting a word to encrypt now possible - beginning of decryption

--- main.c
+++ main.c
@@ -7,23 +7,45 @@ Words can only be a single word or the program will exit.
 Words must be in capital letters
 Program must be run from the terminal section */
 
-char encrypt(char *word); // this is the prototype for the function that will handle encrypting the word
-
+char Rencrypt(char *word); // this is the prototype for the function that will handle encrypting the word using a rotational cipher 
+char Rdecrypt(char *word); // this is the prototype for the function that will handle decrypting a rotational cipher
 int main() 
 {
     char word [100]; //initialises string with a number of letters most words won't exceed.  
+    int function = 0; /* f will acts as the integer which chooses out of the multiple functions of this code -
+    i.e., 1 will be rotational cipher encryption, 2 will be rotational cipher decryption, 
+    3 will be sucstitution cipher encryption and 4 will be rotational cipher decryption*/
+  
+  /* the below 5 lines act as the 'user-friendly' manuel in which the user can insert a number
+  from 1-4 to determine which function within the program they would like to use*/
+ 
+        printf("Choose function you wish to use: \n");
+        printf("Insert 1 to encrypt using a rotational cipher \n");
+        printf("Insert 2 to decrypt using a rotational cipher \n");
+      /*  printf("Insert 3 to encrypt using a substitution cipher, \n");
+        printf("Insert 4 to decrypt using a substitution cipher: "); */
+        scanf("%d", &function);
+        
+        switch (function) {
+            case 1:
+            printf("You chose to encrypt a word using a rotational cipher! \n");
+            printf("Insert word to be encrypted (use capital letters):"); // prompts the user to input word
+            scanf("%s", word); //scans the word wished to be encrypted and puts it in the string. 
+            
+                Rencrypt(word); // the line of code indicating the string 'word' will be used in the encrypt function. 
     
-        printf("Insert word to be encrypted (use capital letters):"); // prompts the user to input word
-        scanf("%s", word); //scans the word wished to be encrypted and puts it in the string. 
-
-        encrypt(word); // the line of code indicating the string 'word' will be used in the encrypt function. 
-    
+        }
+            case 2:
+            printf("You chose to decrypt a word using a rotational cipher! \n");
+            printf("Insert encrypted word to be decrypted (use capital letters): ");
+            scanf("%s", word);
+            
     return 0; 
 }
 
 // the following function will define how the word is encrypted using a rotation cipher
 
-char encrypt(char *word) {
+char Rencrypt(char *word) {
     char i = 0; // a counter to consecutively encrypt the letters within the inserted word 
     int k = 0; // this is the 'key', being the number of 'shifts' the letter will move - this will rotate k letters 
     int n = 0; // this is an integer for the number of letters in the word - used to kill the loop when the word is dont being encrypted
